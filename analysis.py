e44203f2c2bae039cbc9afafaa9d3707f78dbfea
Mellenseed <piano.ellen.foreman@gmail.com>
2025-04-09 09:00:53 -0400

    added compiled list of repertoire and cleaned data

--- analysis.py
+++
@@ -1,6 +0,0 @@
-import re
-from pdfminer.high_level import extract_pages, extract_text
-text = extract_text("sample.pdf")
-pattern = re.compile(r"[a-zA-z]+,{1}\s{1}")
-matches = pattern.findall(text)
-print(matches)
